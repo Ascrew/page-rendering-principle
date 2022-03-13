a4528a332ad661f7eb7ede49f56b818a21cbf832
mypc_wsl <1311585620@qq.com>
2022-03-13 10:50:28 +0800

    done

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
+// app.js
 const express = require('express')
 const fs = require('fs')
 const app = new express()
 const port = 3000
 
 const sleepFun = time => {
-  return new Promise( res => {
+  return new Promise(res => {
     setTimeout(() => {
       res()
     }, time)
   })
 }
+
+const filter = (req, res, next) => {
+  const { sleep } = req.query || 0
+
+  if (sleep) {
+    sleepFun(sleep).then(() => next())
+  } else {
+    next()
+  }
+}
+
+app.use(filter)
+
+app.use('/static/', express.static('./static/'))
+
+app.get('/', function (req, res, next) {
+  fs.readFile('./index.html', 'UTF-8', (err, data) => {
+    if (err) return
+
+    res.send(data)
+  })
+})
+
+app.listen(port, () => {
+  console.log(`app is running at http://127.0.0.1:${port}/`)
+})
---
+++ static/index.js
@@ -0,0 +1,3 @@
+// static/index.js
+var p = document.querySelector('p')
+console.log('listen p in index.js', p)
